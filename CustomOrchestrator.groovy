7a0f14710d52fc4f3b6be2d5d09ad6d0491236c4
xanderLum <xander.lumapac@gmail.com>
2022-08-11 12:42:08 +0800

    add

--- CustomOrchestrator.groovy
+++ CustomOrchestrator.groovy
@@ -1,39 +1,36 @@
 final chaosFramework = library('chaos-framework-library')
 
-
 def call() {
-    stages {
-        stage('cleanWs') {
-            steps {
-                script {
-                    cleanWs()
-                }
+    stage('cleanWs') {
+        steps {
+            script {
+                cleanWs()
             }
         }
-        stage('checkout') {
-            steps {
-                script {
-                    checkout scm
-                }
+    }
+    stage('checkout') {
+        steps {
+            script {
+                checkout scm
             }
         }
-        stage('Inject JBoss Server in Group A with 25% CPU and 25% RAM for 10 seconds') {
-            steps {
-                script {
-                    try {
-                        echo "loading chaos-framework-library utility.ChaosUtil"
-                        def chaosUtil = chaosFramework.utility.ChaosUtil
-                        echo "loading chaos-framework-library utility.VMWareMangle.ReqMapper.CPURequestObj"
-                        def reqMapper = chaosFramework.utility.VMWareMangle.ReqMapper.CPURequestObj.new("remote-tomcat", null, "remote-tomcat", null, "cpu")
-                        echo "cpuReq: ${reqMapper}"
+    }
+    stage('Inject JBoss Server in Group A with 25% CPU and 25% RAM for 10 seconds') {
+        steps {
+            script {
+                try {
+                    echo "loading chaos-framework-library utility.ChaosUtil"
+                    def chaosUtil = chaosFramework.utility.ChaosUtil
+                    echo "loading chaos-framework-library utility.VMWareMangle.ReqMapper.CPURequestObj"
+                    def reqMapper = chaosFramework.utility.VMWareMangle.ReqMapper.CPURequestObj.new("remote-tomcat", null, "remote-tomcat", null, "cpu")
+                    echo "cpuReq: ${reqMapper}"
 
-                        echo "Injecting Cpu fault"
-                        echo "cpuRequestObj : ${cpuRequestObj}"
-                        echo "executing ${chaosFramework.utility.ChaosUtil.injectCPUFault(this, cpuRequestObj)}"
+                    echo "Injecting Cpu fault"
+                    echo "cpuRequestObj : ${cpuRequestObj}"
+                    echo "executing ${chaosFramework.utility.ChaosUtil.injectCPUFault(this, cpuRequestObj)}"
 
-                    } catch (Exception e) {
-                        println e.getMessage()
-                    }
+                } catch (Exception e) {
+                    println e.getMessage()
                 }
             }
         }
