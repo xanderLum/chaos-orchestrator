2739bd37d000fe5acdb71e420ed984e24f0fbc5d
xanderLum <xander.lumapac@gmail.com>
2022-08-11 11:52:08 +0800

    retry

--- CustomOrchestrator.groovy
+++ CustomOrchestrator.groovy
@@ -1,11 +1,3 @@
-/*library('chaos-framework-library').constants.APIGroovy
-library('chaos-framework-library').constants.Credentials
-library('chaos-framework-library').utility.APIReqBuilder
-library('chaos-framework-library').utility.ChaosUtil
-library('chaos-framework-library').utility.VMWareMangle.ReqMapper.CPURequestObj
-library('chaos-framework-library').utility.VMWareMangle.ReqMapper.DiskIORequestObj
-library('chaos-framework-library').utility.VMWareMangle.ReqMapper.MemoryRequestObj*/
-
 final chaosFramework = library('chaos-framework-library')
 
 /**
@@ -15,41 +7,28 @@ final chaosFramework = library('chaos-framework-library')
  * @return
  */
 def call() {
-    node {
-        /*stage('Clean Workspace') {
-            cleanWs()
-        }
-        stage('Checkout') {
-            checkout scm
-        }*/
-
-        stage('Inject JBoss Server in Group A with 25% CPU and 25% RAM for 10 seconds') {
-            try {
-                echo "loading chaos-framework-library utility.ChaosUtil"
-                def chaosUtil = chaosFramework.utility.ChaosUtil
-                echo "loading chaos-framework-library utility.VMWareMangle.ReqMapper.CPURequestObj"
-                def reqMapper = chaosFramework.utility.VMWareMangle.ReqMapper.CPURequestObj.new("remote-tomcat", null, "remote-tomcat", null, "cpu")
+//    node {
+    stage('Inject JBoss Server in Group A with 25% CPU and 25% RAM for 10 seconds') {
+        try {
+            echo "loading chaos-framework-library utility.ChaosUtil"
+            def chaosUtil = chaosFramework.utility.ChaosUtil
+            echo "loading chaos-framework-library utility.VMWareMangle.ReqMapper.CPURequestObj"
+            def reqMapper = chaosFramework.utility.VMWareMangle.ReqMapper.CPURequestObj.new("remote-tomcat", null, "remote-tomcat", null, "cpu")
 //                def cpuRequestObj = reqMapper.CPURequestObj.new("remote-tomcat", null, "remote-tomcat", null, "cpu")
-                echo "cpuReq: ${reqMapper}"
+            echo "cpuReq: ${reqMapper}"
 //                def cpuRequestObj = library('chaos-framework-library').utility.VMWareMangle.ReqMapper.CPURequestObj.new("remote-tomcat", null, "remote-tomcat", null, "cpu")
 
-                //        super(endpointName, timeoutInMilliseconds, id, injectionHomeDir, taskName)
-                echo "Injecting Cpu fault"
-                echo "cpuRequestObj : ${cpuRequestObj}"
-                echo "executing ${chaosFramework.utility.ChaosUtil.injectCPUFault(this, cpuRequestObj)}"
+            //        super(endpointName, timeoutInMilliseconds, id, injectionHomeDir, taskName)
+            echo "Injecting Cpu fault"
+            echo "cpuRequestObj : ${cpuRequestObj}"
+            echo "executing ${chaosFramework.utility.ChaosUtil.injectCPUFault(this, cpuRequestObj)}"
 
-            } catch (Exception e) {
-                println e.getMessage()
-            }
-
-            /*try {
-                ChaosUtil.MemoryFault("groupA", 25, 10)
-            } catch (Exception e) {
-                println e.getMessage()
-            }*/
+        } catch (Exception e) {
+            println e.getMessage()
         }
     }
 }
+//}
 
 return this
 
