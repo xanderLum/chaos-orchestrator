9ccc0ef4e3c0e839eef51c2962e77c80ca505345
xanderLum <xander.lumapac@gmail.com>
2022-08-11 12:20:44 +0800

    refactor

--- CustomOrchestrator.groovy
+++ CustomOrchestrator.groovy
@@ -1,74 +1,74 @@
 final chaosFramework = library('chaos-framework-library')
 
 
-
 def call() {
-    pipeline {
-        agent any
-        stages{
-            stage('cleanWs'){
-                steps{
-                    script{
-                        cleanWs()
-                    }
+    stages {
+        stage('cleanWs') {
+            steps {
+                script {
+                    cleanWs()
                 }
             }
-            stage('checkout'){
-                steps{
-                    script{
-                        checkout scm
-                    }
-                }
-            }
-            stage('Inject JBoss Server in Group A with 25% CPU and 25% RAM for 10 seconds') {
-                try {
-                    echo "loading chaos-framework-library utility.ChaosUtil"
-                    def chaosUtil = chaosFramework.utility.ChaosUtil
-                    echo "loading chaos-framework-library utility.VMWareMangle.ReqMapper.CPURequestObj"
-                    def reqMapper = chaosFramework.utility.VMWareMangle.ReqMapper.CPURequestObj.new("remote-tomcat", null, "remote-tomcat", null, "cpu")
-                    echo "cpuReq: ${reqMapper}"
-
-                    echo "Injecting Cpu fault"
-                    echo "cpuRequestObj : ${cpuRequestObj}"
-                    echo "executing ${chaosFramework.utility.ChaosUtil.injectCPUFault(this, cpuRequestObj)}"
-
-                } catch (Exception e) {
-                    println e.getMessage()
-                }
-            }
-
-            /*stage('Inject JBoss Server in Group A with 25% CPU and 25% RAM for 10 seconds') {
-                steps{
-                    script{
-                        def ochestrator = load 'CustomOrchestrator.groovy'
-                        ochestrator.call()
-                    }
-                }
-            }*/
         }
-        /*  cleanWs()
-         checkout scm
-         orchestrator = load 'CustomOrchestrator.groovy'
-         orchestrator.call() */
+        stage('checkout') {
+            steps {
+                script {
+                    checkout scm
+                }
+            }
+        }
+        stage('Inject JBoss Server in Group A with 25% CPU and 25% RAM for 10 seconds') {
+            steps {
+                script {
+                    try {
+                        echo "loading chaos-framework-library utility.ChaosUtil"
+                        def chaosUtil = chaosFramework.utility.ChaosUtil
+                        echo "loading chaos-framework-library utility.VMWareMangle.ReqMapper.CPURequestObj"
+                        def reqMapper = chaosFramework.utility.VMWareMangle.ReqMapper.CPURequestObj.new("remote-tomcat", null, "remote-tomcat", null, "cpu")
+                        echo "cpuReq: ${reqMapper}"
+
+                        echo "Injecting Cpu fault"
+                        echo "cpuRequestObj : ${cpuRequestObj}"
+                        echo "executing ${chaosFramework.utility.ChaosUtil.injectCPUFault(this, cpuRequestObj)}"
+
+                    } catch (Exception e) {
+                        println e.getMessage()
+                    }
+                }
+            }
+        }
     }
 
     /*stage('Inject JBoss Server in Group A with 25% CPU and 25% RAM for 10 seconds') {
-        try {
-            echo "loading chaos-framework-library utility.ChaosUtil"
-            def chaosUtil = chaosFramework.utility.ChaosUtil
-            echo "loading chaos-framework-library utility.VMWareMangle.ReqMapper.CPURequestObj"
-            def reqMapper = chaosFramework.utility.VMWareMangle.ReqMapper.CPURequestObj.new("remote-tomcat", null, "remote-tomcat", null, "cpu")
-            echo "cpuReq: ${reqMapper}"
-
-            echo "Injecting Cpu fault"
-            echo "cpuRequestObj : ${cpuRequestObj}"
-            echo "executing ${chaosFramework.utility.ChaosUtil.injectCPUFault(this, cpuRequestObj)}"
-
-        } catch (Exception e) {
-            println e.getMessage()
+        steps{
+            script{
+                def ochestrator = load 'CustomOrchestrator.groovy'
+                ochestrator.call()
+            }
         }
     }*/
 }
+/*  cleanWs()
+ checkout scm
+ orchestrator = load 'CustomOrchestrator.groovy'
+ orchestrator.call() */
+}
 
+/*stage('Inject JBoss Server in Group A with 25% CPU and 25% RAM for 10 seconds') {
+    try {
+        echo "loading chaos-framework-library utility.ChaosUtil"
+        def chaosUtil = chaosFramework.utility.ChaosUtil
+        echo "loading chaos-framework-library utility.VMWareMangle.ReqMapper.CPURequestObj"
+        def reqMapper = chaosFramework.utility.VMWareMangle.ReqMapper.CPURequestObj.new("remote-tomcat", null, "remote-tomcat", null, "cpu")
+        echo "cpuReq: ${reqMapper}"
+
+        echo "Injecting Cpu fault"
+        echo "cpuRequestObj : ${cpuRequestObj}"
+        echo "executing ${chaosFramework.utility.ChaosUtil.injectCPUFault(this, cpuRequestObj)}"
+
+    } catch (Exception e) {
+        println e.getMessage()
+    }
+}*/
 return this
 
