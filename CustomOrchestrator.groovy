3779cc76dd08526967f412c9abecee856f9cd284
xanderLum <xander.lumapac@gmail.com>
2022-08-11 12:42:59 +0800

    fix

--- CustomOrchestrator.groovy
+++ CustomOrchestrator.groovy
@@ -2,37 +2,25 @@ final chaosFramework = library('chaos-framework-library')
 
 def call() {
     stage('cleanWs') {
-        steps {
-            script {
-                cleanWs()
-            }
-        }
+        cleanWs()
     }
     stage('checkout') {
-        steps {
-            script {
-                checkout scm
-            }
-        }
+        checkout scm
     }
     stage('Inject JBoss Server in Group A with 25% CPU and 25% RAM for 10 seconds') {
-        steps {
-            script {
-                try {
-                    echo "loading chaos-framework-library utility.ChaosUtil"
-                    def chaosUtil = chaosFramework.utility.ChaosUtil
-                    echo "loading chaos-framework-library utility.VMWareMangle.ReqMapper.CPURequestObj"
-                    def reqMapper = chaosFramework.utility.VMWareMangle.ReqMapper.CPURequestObj.new("remote-tomcat", null, "remote-tomcat", null, "cpu")
-                    echo "cpuReq: ${reqMapper}"
+        try {
+            echo "loading chaos-framework-library utility.ChaosUtil"
+            def chaosUtil = chaosFramework.utility.ChaosUtil
+            echo "loading chaos-framework-library utility.VMWareMangle.ReqMapper.CPURequestObj"
+            def reqMapper = chaosFramework.utility.VMWareMangle.ReqMapper.CPURequestObj.new("remote-tomcat", null, "remote-tomcat", null, "cpu")
+            echo "cpuReq: ${reqMapper}"
 
-                    echo "Injecting Cpu fault"
-                    echo "cpuRequestObj : ${cpuRequestObj}"
-                    echo "executing ${chaosFramework.utility.ChaosUtil.injectCPUFault(this, cpuRequestObj)}"
+            echo "Injecting Cpu fault"
+            echo "cpuRequestObj : ${cpuRequestObj}"
+            echo "executing ${chaosFramework.utility.ChaosUtil.injectCPUFault(this, cpuRequestObj)}"
 
-                } catch (Exception e) {
-                    println e.getMessage()
-                }
-            }
+        } catch (Exception e) {
+            println e.getMessage()
         }
     }
 
