90d76531c3c8e66e999772871f40ca2814361d76
xanderLum <xander.lumapac@gmail.com>
2022-09-19 16:22:33 +0800

    set null on cronExpression param

--- CustomOrchestratorGrp.groovy
+++ CustomOrchestratorGrp.groovy
@@ -12,7 +12,11 @@ def call() {
     stage('Inject DB MySQL Server in endpoint javaapp with 100% CPU and 90% Memory parallel for 35 minutes') {
         try {
             echo "Injecting CPU fault"
-            echo "executing ${chaosUtil.injectCPUFault(this, "javaapp", 2.1e+6, "javaapp", null, "dbgrp-iteration1-cpu", 100, false, "", 1, "schedId-1", "schedulingTest")}"
+            //injectCPUUtil Params(
+            //                  String endpointName, def timeoutInMilliseconds, String id, String injectionHomeDir,
+            //                  String taskName, def cpuLoad, randomEndpoint,
+            //                  String cronExpression, def schedTimeoutInMilliseconds, String schedId, String description)
+            echo "executing ${chaosUtil.injectCPUFault(this, "javaapp", 2.1e+6, "javaapp", null, "dbgrp-iteration1-cpu", 100, false, null, 1, "schedId-1", "schedulingTest")}"
 
         } catch (Exception e) {
             println e.getMessage()
@@ -20,7 +24,7 @@ def call() {
 
         try {
             echo "Injecting Memory fault"
-            echo "executing ${chaosUtil.injectMemoryFault(this, "javaapp", 2.1e+6, "javaapp", null, "dbgrp-iteration1-memory", 90, false, "", 1, "schedId-1", "schedulingTest")}"
+            echo "executing ${chaosUtil.injectMemoryFault(this, "javaapp", 2.1e+6, "javaapp", null, "dbgrp-iteration1-memory", 90, false, null, 1, "schedId-1", "schedulingTest")}"
 
         } catch (Exception e) {
             println e.getMessage()
@@ -31,14 +35,14 @@ def call() {
 
         try {
             echo "Injecting DiskIO fault"
-            echo "executing ${chaosUtil.injectDiskIOFault(this, "javaapp", 2.1e+6, "javaapp", null, "dbgrp-iteration1-diskIO", 3e+6, "/home/trps.mas.gov.sg/a_osp_chaos", false, "", 1, "schedId-1", "schedulingTest")}"
+            echo "executing ${chaosUtil.injectDiskIOFault(this, "javaapp", 2.1e+6, "javaapp", null, "dbgrp-iteration1-diskIO", 3e+6, "/home/trps.mas.gov.sg/a_osp_chaos", false, null, 1, "schedId-1", "schedulingTest")}"
         } catch (Exception e) {
             println e.getMessage()
         }
 
         try {
             echo "Injecting DiskSpace fault"
-            echo "executing ${chaosUtil.injectDiskSpaceFault(this, "javaapp", 2.1e+6, "javaapp", null, "dbgrp-iteration1-diskSpace", "/home/trps.mas.gov.sg/a_osp_chaos", 90, false, "", 1, "schedId-1", "schedulingTest")}"
+            echo "executing ${chaosUtil.injectDiskSpaceFault(this, "javaapp", 2.1e+6, "javaapp", null, "dbgrp-iteration1-diskSpace", "/home/trps.mas.gov.sg/a_osp_chaos", 90, false, null, 1, "schedId-1", "schedulingTest")}"
 
         } catch (Exception e) {
             println e.getMessage()
